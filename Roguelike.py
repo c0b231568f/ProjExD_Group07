65a0beb485d713dd3575d7e501261547178514d3
hajime <c011918477@edu.teu.ac.jp>
2024-05-21 17:12:08 +0900

    フィールドサイズ分の背景生成

--- Roguelike.py
+++ Roguelike.py
@@ -7,6 +7,7 @@ import pygame as pg
 from pygame.sprite import Group
 
 WIDTH, HEIGHT = 1200, 900
+FIELDRANGE = 1200
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,6 +21,30 @@ def calc_orientation(org: pg.Rect, dst: pg.Rect) -> tuple[float, float]:
     norm = math.sqrt(x_diff ** 2 + y_diff ** 2)
     return x_diff / norm, y_diff / norm
 
+def over_field(obj_rct: pg.Rect) -> bool:
+    """
+    オブジェクトがフィールド外にいったらTrueを返す関数
+    引数：pg.Rect
+    戻り値：T/F
+    """
+    if obj_rct.left < 0 - FIELDRANGE or \
+        WIDTH + FIELDRANGE < obj_rct.right or \
+        obj_rct.top < 0 - FIELDRANGE or \
+        HEIGHT + FIELDRANGE < obj_rct.bottom:
+        return True
+    return False
+
+class Weapon(pg.sprite.Sprite):
+    """
+    Heroクラスをベースに武器にまつわるクラス
+    引数：Heroクラス
+    """
+    def __init__(self, hero):
+        super().__init__()
+
+    def update():
+        pass
+
 def main():
     """
     main関数
@@ -29,18 +54,22 @@ def main():
     pg.display.set_caption("roguelike")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load(f"fig/maptile_sogen_02.png")
+    bg_img = pg.transform.scale(bg_img, (WIDTH/30, HEIGHT/30))
 
     while True:
+        # hero = Hero()
         key_lst = pg.key.get_pressed()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
-        screen.blit(bg_img, [0, 0])
+        total_dist = (110,110)# hero.mvd() # hero.mvdメソッドは、他のメンバーが作る予定（戻り値x, yのタプル）
+        for i in range(90):
+            for j in range(90):
+                screen.blit(bg_img, [-WIDTH + i*WIDTH/30 + (total_dist[0]%(WIDTH/30)), -HEIGHT + j*HEIGHT/30+ (total_dist[1]%(HEIGHT/30))])
 
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
