d4739295592a32b36492a3ad13e0c61b390de41a
C0A23002 <c0a23002f0@edu.teu.ac.jp>
2024-05-28 13:30:16 +0900

    主人公完成

--- Roguelike.py
+++ Roguelike.py
@@ -29,29 +29,30 @@ class Hero:
     """
     spd = 1.0
     delta = {
-        pg.K_UP: (0, -1*spd),
-        pg.K_RIGHT: (1*spd, 0),
-        pg.K_DOWN: (0, 1*spd),
-        pg.K_LEFT: (-1*spd, 0),
+        pg.K_UP: (0, -1),
+        pg.K_RIGHT: (1, 0),
+        pg.K_DOWN: (0, 1),
+        pg.K_LEFT: (-1, 0),
     }
     img0 = pg.transform.rotozoom(pg.image.load("fig/hero0.png"), 0, 0.1)
     img = pg.transform.flip(img0, True, False)
     imgs = {
-        (+1*spd, 0): img0,  # 右
-        (+1*spd, -1*spd): img0,  # 右上
-        (0, -1*spd): img0,  # 上
-        (-1*spd, -1*spd): img,  # 左上
-        (-1*spd, 0): img,  # 左
-        (-1*spd, +1*spd): img,   # 左下
-        (0, +1*spd): img,  # 下
-        (+1*spd, +1*spd): img0,  # 右下
+        (+1, 0): img0,  # 右
+        (+1, -1): img0,  # 右上
+        (0, -1): img0,  # 上
+        (-1, -1): img,  # 左上
+        (-1, 0): img,  # 左
+        (-1, +1): img,   # 左下
+        (0, +1): img,  # 下
+        (+1, +1): img0,  # 右下
     }
 
     def __init__(self, xy: tuple[int, int]):
         """
         Surfaceつくる
         """
-        self.img0 = __class__.imgs[(+1, 0)]
+        self.spd = __class__.spd
+        self.img0 = self.imgs[(+1, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
 
@@ -63,13 +64,13 @@ class Hero:
         self.spd = spd
         for k, mv in __class__.delta.items():
             if key_lst[k]:
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
+                sum_mv[0] += mv[0]*spd
+                sum_mv[1] += mv[1]*spd
         self.rct.move_ip(sum_mv)
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
-        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
-            self.img = __class__.imgs[tuple(sum_mv)]
+        if any(sum_mv): # sum_mv両方0の時のみFalse
+            self.img = __class__.imgs[tuple([_/abs(_) if _ != 0 else 0 for _ in sum_mv])] # __class__.imgsのkeyの値に整形
         screen.blit(self.img, self.rct)
 
 
@@ -86,20 +87,118 @@ class Hero:
         return ttl_mv
 
 
+import math
+import os
+import random
+import sys
+import time
+import pygame as pg
+from pygame.sprite import Group
+
+WIDTH, HEIGHT = 1200, 900
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    画面内判定 : 中のときTrue
+    """
+    yoko, tate = True, True
+    if obj_rct.right < 0 or WIDTH < obj_rct.left:
+        yoko = False
+    if obj_rct.bottom < 0 or HEIGHT < obj_rct.top:
+        tate = False
+    return yoko, tate
+
+
+class Hero:
+    """
+    主人公に関するクラス
+    """
+    spd = 1.0
+    delta = {
+        pg.K_UP: (0, -1),
+        pg.K_RIGHT: (1, 0),
+        pg.K_DOWN: (0, 1),
+        pg.K_LEFT: (-1, 0),
+    }
+    
+    img0 = pg.transform.rotozoom(pg.image.load("fig/hero0.png"), 0, 0.15)
+    img = pg.transform.flip(img0, True, False)
+
+    imgs = {
+        (+1, 0): img0,  # 右
+        (+1, -1): img0,  # 右上
+        (0, -1): img0,  # 上
+        (-1, -1): img,  # 左上
+        (-1, 0): img,  # 左
+        (-1, +1): img,   # 左下
+        (0, +1): img,  # 下
+        (+1, +1): img0,  # 右下
+    }
+
+    def __init__(self, xy: tuple[int, int]):
+        """
+        Surfaceつくる
+        """
+        self.spd = __class__.spd
+        self.img0 = self.imgs[(+1, 0)]
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = xy
+
+
+    def update(self, key_lst: list[bool], spd: float, screen: pg.Surface):
+        """
+        移動させる
+        """
+        sum_mv = [0, 0]
+        self.spd = spd
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]*spd
+                sum_mv[1] += mv[1]*spd
+        self.rct.move_ip(sum_mv)
+        if check_bound(self.rct) != (True, True):
+            self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if any(sum_mv): # sum_mv両方0の時のみFalse
+            self.img = __class__.imgs[tuple([_/abs(_) if _ != 0 else 0 for _ in sum_mv])] # __class__.imgsのkeyの値に整形
+        screen.blit(self.img, self.rct)
+
+
+    def mvd(self, key_lst: list[bool], spd: float) -> tuple[int, int]:
+        """
+        総移動距離
+        """
+        ttl_mv = [0, 0]
+        self.spd = spd
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                ttl_mv[0] += mv[0]
+                ttl_mv[1] += mv[1]
+        return ttl_mv
+
+
 def main():
     tmr = 0
-    spd = 1.0
+    spd = 10.0
     hp = 100
     clock = pg.time.Clock()
     pg.display.set_caption("roguelike")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"fig/maptile_sogen_02.png")
+    bg_img = pg.transform.scale(bg_img, (WIDTH, HEIGHT))
     hero = Hero((WIDTH/2, HEIGHT/2))
 
     while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0
+        screen.blit(bg_img, (0, 0))
         if hp > 0:
             key_lst = pg.key.get_pressed()
-            hero.update(key_lst, spd, screen)
         total_moved = hero.mvd(key_lst, spd)
+        hero.update(key_lst, spd, screen)
         pg.display.update()
         clock.tick(50)
 
